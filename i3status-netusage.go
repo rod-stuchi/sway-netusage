706e6a217b6a320cf9d73585551b50737b0d419d
cmprmsd <73472903+cmprmsd@users.noreply.github.com>
2022-04-08 01:27:16 +0200

    Waybar release
    * add auto detection of gateway if no interface is given (not sure if the method is correct though)
    * change code to use with Waybar
    * change bytes to B
    * remove fraction numbers for KiB

--- i3status-netusage.go
+++ i3status-netusage.go
@@ -2,20 +2,21 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"os"
+	"net"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
+
+	"golang.org/x/net/nettest"
 )
 
 var (
-	iface = flag.String("interface", "eth0", "What interface to use")
+	iface = flag.String("interface", "", "What interface to use")
 )
 
 // stats fetches the cumulative rx/tx bytes for network interface
@@ -47,50 +48,45 @@ func stats() (rx, tx uint64) {
 	return 0, 0
 }
 
+func getRoutedInterface() *net.Interface {
+	val, _ := nettest.RoutedInterface("ip", net.FlagUp|net.FlagBroadcast)
+	return val
+}
+
 // format converts a number of bytes in KiB or MiB.
 func format(counter, prevCounter uint64, window float64) string {
 	if prevCounter == 0 {
-		return "bytes"
+		return "B"
 	}
 	r := float64(counter-prevCounter) / window
 	if r < 1024 {
-		return fmt.Sprintf("%.0f bytes", r)
+		return fmt.Sprintf("%.0f B", r)
 	}
 	if r < 1024*1024 {
-		return fmt.Sprintf("%.1f KiB", r/1024)
+		return fmt.Sprintf("%.0f KiB", r/1024)
 	}
-	return fmt.Sprintf("%.1f MiB", r/1024/1024)
+	return fmt.Sprintf("%.0f MiB", r/1024/1024)
 }
 
 func main() {
 	flag.Parse()
-
+	if *iface == "" {
+		autoInterface := getRoutedInterface().Name
+		fmt.Printf("Auto detected %s\n", autoInterface)
+		*iface = autoInterface
+	}
 	prevRx, prevTx := uint64(0), uint64(0)
-	bio := bufio.NewReader(os.Stdin)
 	prev := time.Now()
 	for {
-		line, err := bio.ReadString('\n')
-		if err != nil {
-			os.Exit(1)
-		}
+
+		time.Sleep(1 * time.Second)
 		now := time.Now()
 		window := now.Sub(prev).Seconds()
 		prev = now
-
-		prefix := ""
-		if strings.HasPrefix(line, ",[{") {
-			prefix = ","
-			line = line[1:]
-		}
-		if !(strings.HasPrefix(line, "[{") && strings.HasSuffix(line, "}]\n")) {
-			fmt.Print(prefix, line)
-			continue
-		}
-		line = line[1:]
 		rx, tx := stats()
 		rxRate := format(rx, prevRx, window)
 		txRate := format(tx, prevTx, window)
-		fmt.Printf("%s[{\"full_text\":\"%10s/s↓ %10s/s↑\"},%s", prefix, rxRate, txRate, line)
+		fmt.Printf("%8s/s ↓ %8s/s ↑\n", rxRate, txRate)
 		prevRx, prevTx = rx, tx
 	}
 
